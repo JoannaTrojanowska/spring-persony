09d1ed8033d056a7a21d3c4c797f77ee1d8fe239
joannatrojanowska <joasia.trojanowska@gmail.com>
2019-12-09 15:36:48 +0100

    Zrobiłam projekt w Springu o personach. Zrobilam mapowanie do tabelki.

---
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Person;
+import com.example.demo.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping("/moje-persony")
+    public Iterable<Person> mojePersony(){
+        return personRepository.findAll();
+    }
+}
